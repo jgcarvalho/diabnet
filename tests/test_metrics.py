6791c342070feb410b267f665da1b969594d1f4d
jvsguerra <jvsguerra@gmail.com>
2021-05-21 17:58:48 -0300

    Update only valid tests

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -2,15 +2,16 @@ import unittest
 import numpy as np
 from diabnet.metrics import ece, mce
 
+
 class TestMetrics(unittest.TestCase):
     def setUp(self):
-        self.targets = np.arange(0.0, 1.0, .01)
-        self.predictions = np.arange(0.0, 1.0, .01)
-    
+        self.targets = np.arange(0.0, 1.0, 0.01)
+        self.predictions = np.arange(0.0, 1.0, 0.01)
+
     def test_ece(self):
         result = ece(self.targets, self.predictions, 10)
         self.assertEqual(result, 0.0)
-    
+
     def test_mce(self):
         result = mce(self.targets, self.predictions, 10)
         self.assertEqual(result, 0.0)
