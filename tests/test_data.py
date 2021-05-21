88811e505963debe957e1f009eb519bd5400d646
jvsguerra <jvsguerra@gmail.com>
2021-05-21 20:43:48 -0300

    Add tests for encode_features and _check_parents_diag functions

--- tests/test_data.py
+++ tests/test_data.py
@@ -28,6 +28,74 @@ class TestData(unittest.TestCase):
             expected.remove(feature)
         self.assertListEqual(result, expected)
 
+    def test_encode_features(self):
+        # Test snp
+        # Dominant homozygous
+        result = data.encode_features(["snp"], [0]).tolist()
+        self.assertListEqual(result, [[2.0], [0.0]])
+        # Heterozygous
+        result = data.encode_features(["snp"], [1]).tolist()
+        self.assertListEqual(result, [[1.0], [1.0]])
+        # Recessive homozygous
+        result = data.encode_features(["snp"], [2]).tolist()
+        self.assertListEqual(result, [[0.0], [2.0]])
+        # Test AGE
+        result = data.encode_features(["AGE"], [20]).tolist()
+        self.assertListEqual(result, [[0.4], [1.0]])
+        result = data.encode_features(["AGE"], [40]).tolist()
+        self.assertListEqual(result, [[0.8], [1.0]])
+        result = data.encode_features(["AGE"], [60]).tolist()
+        self.assertListEqual(result, [[1.2], [1.0]])
+        result = data.encode_features(["AGE"], [80]).tolist()
+        self.assertListEqual(result, [[1.6], [1.0]])
+        # Test sex
+        result = data.encode_features(["sex"], ["F"]).tolist()
+        self.assertListEqual(result, [[2.0], [0.0]])
+        result = data.encode_features(["sex"], ["M"]).tolist()
+        self.assertListEqual(result, [[0.0], [2.0]])
+        # Test mo_t2d and fa_t2d
+        result = data.encode_features(["snp", "mo_t2d", "fa_t2d"], [0, 0, 0]).tolist()
+        self.assertListEqual(
+            result,
+            [[2.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0], [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]],
+        )
+        result = data.encode_features(["snp", "mo_t2d", "fa_t2d"], [0, 0, 1]).tolist()
+        self.assertListEqual(
+            result,
+            [[2.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0], [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]],
+        )
+        result = data.encode_features(["snp", "mo_t2d", "fa_t2d"], [0, 0, 2]).tolist()
+        self.assertListEqual(
+            result,
+            [[2.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0], [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]],
+        )
+        result = data.encode_features(["snp", "mo_t2d", "fa_t2d"], [0, 1, 0]).tolist()
+        self.assertListEqual(
+            result,
+            [[2.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0], [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]],
+        )
+        result = data.encode_features(["snp", "mo_t2d", "fa_t2d"], [0, 2, 0]).tolist()
+        self.assertListEqual(
+            result,
+            [[2.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0], [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]],
+        )
+        # Test unknown feature
+        self.assertRaises(ValueError, data.encode_features, ["unknown-feature"], [0.0])
+
+    def test_check_parents_diag(self):
+        # Wrong order for 'mo_t2d' and 'fa_t2d'
+        self.assertRaises(
+            ValueError, data._check_parents_diag, ["snp", "fa_t2d", "mo_t2d"]
+        )
+        self.assertRaises(
+            ValueError, data._check_parents_diag, ["mo_t2d", "fa_t2d", "snp"]
+        )
+        self.assertRaises(
+            ValueError, data._check_parents_diag, ["fa_t2d", "mo_t2d", "snp"]
+        )
+        # Correct order
+        self.assertIsNone(data._check_parents_diag(["mo_t2d", "fa_t2d"]))
+
 
 # class TestDiabDataset(unittest.TestCase):
 #     def test_n_feat(self):
