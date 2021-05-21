f12a81bc765d9864d01d9b345068021e0c4a15d9
jvsguerra <jvsguerra@gmail.com>
2021-05-21 19:32:03 -0300

    Update data.py documentation and functions

--- diabnet/data.py
+++ diabnet/data.py
@@ -25,6 +25,17 @@ def get_feature_names(
     Returns:
     List[str]
         A list with feature names that will be used by DiabNet.
+
+    Raises
+    ------
+    TypeError
+        `fn` must be a string.
+    ValueError
+        `fn` must be a CSV-formatted file.
+    TypeError
+        `use_sex` must be a boolean.
+    TypeError
+        `use_parents_diagnosis` must be a boolean.
     """
     # Check arguments
     if type(fn) not in [str]:
@@ -80,134 +91,250 @@ def get_feature_names(
 
 
 def encode_features(feat_names: List[str], feat_values: List[Any]) -> np.ndarray:
+    """Encode features into a numpy.ndarray to be used as input to DiabNet.
+
+    Parameters
+    ----------
+    feat_names : List[str]
+        A list of feature names.
+    feat_values : List[Any]
+        A list of feature values.
+
+    Returns
+    -------
+    np.ndarray
+        A numpy.ndarray with shape (2, len_encoded_features) for feature
+        values to be used as input to DiabNet.
+
+    Raises
+    ------
+    ValueError
+        Unknown feature: `{feat_name}`.
     """
-    Encode features into a matrix to be used as input.
-    Args:
-        feat_names (List[str]):
-        feat_values (List[Any]):
-
-    Raises:
-        Unknown feature name.
-
-    Returns:
-        Matrix (np.ndarray) with shape (2, len_encoded_features) with feature values
-        to be used as input.
-
-    """
+    # Check if parents diagnosis is in the correct position
+    # FIXME: Check redundancy - use in other functions
     _check_parents_diag(feat_names)
+
+    # Create a zeroed numpy.ndarray for all valid features
     m = np.zeros((2, _len_encoding(feat_names)))
+
+    # Fill numpy.ndarray with encoded feature values
     for i, feat_name in enumerate(feat_names):
+        # NOTE: All encoded feature values must be in the range ~[0, 2]
+
+        # AGE
         if feat_name == "AGE":
-            # print("AGE == {}".format(feat_values[i]/50 ))
-            m[0, i] = feat_values[i] / 50  # divide to 50 to put AGE in the range ~[0,2]
-            m[1, i] = 1  # works like a bias in a layer without bias
-        elif feat_name == "BMI":
-            m[0, i] = feat_values[i] / 10  # divide to 10 to put BMI in the range ~[0,2]
-            m[1, i] = 1  # works like a bias in a layer without bias
+            # Encoding: divide to 50 to put AGE in the range ~[0,2]
+            m[0, i] = feat_values[i] / 50
+            # Works like a bias in a layer without bias
+            m[1, i] = 1
+
+        # SEX
         elif feat_name == "sex":
+            # Enconding: keep sex in the range [0,2]
             if feat_values[i] == "F":
-                m[0, i] = 2  # keep feat in the range [0,2]
+                m[0, i] = 2
             elif feat_values[i] == "M":
                 m[1, i] = 2
+
+        # Mother T2D diagnosis
         elif feat_name == "mo_t2d":
-            # ! this is HIGHLY depedent of '_fix_parents_feat'
-            # if parent diagnosis are included in features both 'mo_t2d' and 'fa_t2d' must be present
-            # AND the sequence must be 'mo_t2d' followed by 'fa_t2d' at the two last positions
-            m[1, i], m[1, i + 1], m[1, i + 2] = 1, 1, 1  # works like a bias
+            # If parent diagnosis are included in features,
+            #  both 'mo_t2d' and 'fa_t2d' must be present.
+            # The sequence must be ['mo_t2d', 'fa_t2d'] in
+            #  the two last positions
+            # Encoding
             if feat_values[i] == 0:
                 m[0, i] = 1  # keep feat in the range [0,2]
             elif feat_values[i] == 1:
                 m[0, i + 1] = 1
             else:
                 m[0, i + 2] = 1
+            # Works like a bias in a layer without bias
+            m[1, i], m[1, i + 1], m[1, i + 2] = 1, 1, 1
+
+        # Father T2D diagnosis
         elif feat_name == "fa_t2d":
-            # ! this is HIGHLY depedent of '_fix_parents_feat' and the presence of 'mo_t2d'
-            # if parent diagnosis are included in features both 'mo_t2d' and 'fa_t2d' must be present
-            # AND the sequence must be 'mo_t2d' followed by 'fa_t2d' at the two last positions
-            m[1, i + 2], m[1, i + 3], m[1, i + 4] = 1, 1, 1  # works like a bias
+            # If parent diagnosis are included in features,
+            #  both 'mo_t2d' and 'fa_t2d' must be present.
+            # The sequence must be ['mo_t2d', 'fa_t2d'] in
+            #  the two last positions
+            # Encoding
             if feat_values[i] == 0:
                 m[0, i + 2] = 1
             elif feat_values[i] == 1:
                 m[0, i + 3] = 1
             else:
                 m[0, i + 4] = 1
+            # Works like a bias in a layer without bias
+            m[1, i + 2], m[1, i + 3], m[1, i + 4] = 1, 1, 1
+
+        # SNPs
         elif feat_name[:3] == "snp":
+            # Dominant homozygous
             if feat_values[i] == 0:
                 m[0, i] = 2
+            # Recessive homozygous
             elif feat_values[i] == 2:
                 m[1, i] = 2
+            # Heterozygous
             else:
                 m[0, i], m[1, i] = 1, 1
+
+        # Unknown feature -> Raise ValueError
         else:
             # if feature name is invalid raise an exception
-            raise Exception(f'Feature name "{feat_name}" is unknown')
+            raise ValueError(f"Unknown feature: `{feat_name}`.")
+
     return m
 
 
-def _check_parents_diag(feat_names: List[str]):
-    # check if 'mo_t2d' followed by 'fa_t2d' are at the two last positions THEY MUST BE
-    if "mo_t2d" in feat_names and "fa_t2d" in feat_names:
+def _check_parents_diag(feat_names: List[str]) -> None:
+    """Checks if parents' diagnosis are in the correct position.
+    The 'mo_t2d' and 'fa_t2d' MUST be the two last position in this
+    specific order.
+
+    Parameters
+    ----------
+    feat_names : List[str]
+        A list of feature names.
+
+    Raises
+    ------
+    ValueError
+        mo_t2d is at index `x` and fa_t2d is at index `y` from
+        feat_names with `n` items. They must be in the penultimate
+        and last positions, respectively.
+    """
+    # If parents diagnosis are in the list of feature names,
+    # check their position
+    if "mo_t2d" and "fa_t2d" in feat_names:
         if feat_names[-2] != "mo_t2d" or feat_names[-1] != "fa_t2d":
-            raise Exception(
-                "mo_t2d is at index {} and fa_t2d is at index {} from \
-                feat_names with {} items".format(
-                    feat_names.index("mo_t2d"),
-                    feat_names.index("fa_t2d"),
-                    len(feat_names),
-                )
+            x = feat_names.index("mo_t2d")
+            y = feat_names.index("fa_t2d")
+            n = len(feat_names)
+            raise ValueError(
+                f"mo_t2d is at index {x} and fa_t2d is at index {y} from \
+                    feat_names with {n} items. They must be in the \
+                        penultimate and last positions, respectively."
             )
 
 
 def _len_encoding(feat_names: List[str]) -> int:
-    # mo_t2d and fa_t2d use 3 columns (+2 each one = 4)
-    if "mo_t2d" in feat_names and "fa_t2d" in feat_names:
+    """Gets length of encoded features
+
+    Parameters
+    ----------
+    feat_names : List[str]
+        A list of feature names
+
+    Returns
+    -------
+    int
+        Length of encoded features
+    """
+    # If 'mo_t2d' and 'fa_t2d' header are in feature names,
+    # both use 3 columns. So, +2 each one, totally 4 extra columns.
+    if "mo_t2d" and "fa_t2d" in feat_names:
         return len(feat_names) + 4
 
     return len(feat_names)
 
 
+# TODO:
+# - get_features_names() inside DiabDataset.__init__() and remove
+#  `feat_names` parameter.
 class DiabDataset(Dataset):
-    """
+    """DiabNet Dataset.
 
-    Args:
-        fn_csv (str): CSV filename with data
-        feat_names (List[str]): feature names that will be used
-        label_name (str): feature name (csv column name) that identify the label
-        soft_label (bool): use soft label. Prob_negatives > 0 and Prob_positives < 1. Default: True
-        soft_label_baseline (float): value to Prob_negatives. Default: 0.0
-        soft_label_topline (float): value to Prob_positives. Default: 1.0
-        soft_label_baseline_slope (float): decrease in Prob_negatives uncertainty. Unevenness between 20yo and 80 yo. Default: 0.0
-        device (str): tensor device. Default is 'cuda'.
-
-    Attributes:
-        feat_names (List[str]):
-        n_feat (int):
-        raw_values (np.ndarray):
-        features (tensor):
-        labels (tensor):
-
+    Attributes
+    ----------
+    feat_names : List[str]
+        Feature names.
+    n_feat : int
+        Number of encoded features.
+    features : torch.Tensor
+        Encoded features.
+    labels : torch.Tensor
+        Target labels.
+    _raw_values : np.ndarray
+        Raw feature values.
+    _raw_labels : np.ndarray
+        Raw target labels.
+    _ages : np.ndarray
+        Raw ages
     """
 
     def __init__(
         self,
-        fn_csv: str,
+        fn: str,
         feat_names: List[str],
-        label_name="T2D",
-        soft_label=True,
-        soft_label_baseline=0.0,
-        soft_label_topline=1.0,
-        soft_label_baseline_slope=0.0,
-        device="cuda",
+        label_name: str = "T2D",
+        soft_label: bool = True,
+        soft_label_baseline: float = 0.0,
+        soft_label_topline: float = 1.0,
+        soft_label_baseline_slope: float = 0.0,
+        device: str = "cuda",
     ):
+        """
+        Parameters
+        ----------
+        fn : str
+            Path to CSV file with data. The first line must have feature names.
+        feat_names : List[str]
+            A list of feature names to be used in DiabNet.
+        label_name : str, optional
+            Feature name (header in `fn`) that identify the target label,
+            by default "T2D".
+        soft_label : bool, optional
+            Whether to use soft label. Negative probabilities > 0.0 and
+            Positive probabilities < 1.0, by default True.
+        soft_label_baseline : float, optional
+            Value to negative probabilities, by default 0.0.
+        soft_label_topline : float, optional
+            Value to positive probabilities, by default 1.0.
+        soft_label_baseline_slope : float, optional
+            Decrease in negative probabilities uncertainty. Unevenness between
+            20 yo and 80 yo, by default 0.0.
+        device : str, optional
+            Tensor device, by default "cuda".
+        """
+        # Check arguments
+        if type(fn) not in [str]:
+            raise TypeError("`fn` must be a string.")
+        if not fn.endswith(".csv"):
+            raise ValueError("`fn` must be a CSV-formatted file.")
+        if type(label_name) not in [str]:
+            raise TypeError("`label_name` must be a string.")
+        if type(soft_label) not in [bool]:
+            raise TypeError("`soft_label` must be a boolean.")
+        if type(soft_label_baseline) not in [float]:
+            raise TypeError("`soft_label_baseline` must be a floating poin.")
+        if type(soft_label_topline) not in [float]:
+            raise TypeError("`soft_label_topline` must be a floating point.")
+        if type(soft_label_baseline_slope) not in [float]:
+            raise TypeError("`soft_label_baseline` must be a floating point.")
+        if type(device) not in [str]:
+            raise TypeError("`device` must be a string.")
 
-        df = pd.read_csv(fn_csv)
+        # Read data from CSV-formatted file
+        df = pd.read_csv(fn)
+
+        # Check if parents diagnosis is in the correct position
         _check_parents_diag(feat_names)
 
+        # ! Attributes ! #
+        # Feature names
         self.feat_names = feat_names
+        # Number of encoded features
         self.n_feat = _len_encoding(feat_names)
+        # Raw feature values
         self._raw_values = df[feat_names].values
+        # Raw target labels
         self._raw_labels = df[label_name].values
+        # Raw ages
         self._ages = df["AGE"].values
+        # Encoded features in torch.Tensor object
         self.features = torch.tensor(
             [
                 encode_features(self.feat_names, raw_value)
@@ -215,15 +342,18 @@ class DiabDataset(Dataset):
             ],
             dtype=torch.float,
         ).to(device)
-
+        # Prepare target labels
         if soft_label:
+            # Use soft labels on target labels
             labels = self._soft_label(
                 baseline=soft_label_baseline,
                 topline=soft_label_topline,
                 baseline_slope=soft_label_baseline_slope,
             )
         else:
+            # Use target labels
             labels = self._raw_labels
+        # Change shape from (n, ) to (n, 1)
         self.labels = torch.unsqueeze(torch.tensor(labels, dtype=torch.float), 1).to(
             device
         )
@@ -231,67 +361,107 @@ class DiabDataset(Dataset):
     def _soft_label(
         self, baseline: float, topline: float, baseline_slope: float
     ) -> np.ndarray:
-        assert baseline < topline and baseline_slope <= 0.0
+        """Convert raw target labels to soft labels.
+
+        Parameters
+        ----------
+        baseline : float
+            Value to negative probabilities
+        topline : float
+            Value to positive probabilities
+        baseline_slope : float
+            Decrease in negative probabilities uncertainty. Unevenness between
+            20 yo and 80 yo.
+
+        Returns
+        -------
+        np.ndarray
+            A numpy.ndarray with soft labels.
+
+        Raises
+        ------
+        ValueError
+            `soft_label_topline` must be greater than `soft_label_baseline`.
+        ValueError
+            `soft_label_baseline_slope` must be a negative floating point or
+            zero.
+        """
+        # Check arguments
+        if baseline >= topline:
+            raise ValueError(
+                "`soft_label_topline` must be greater than \
+                    `soft_label_baseline`."
+            )
+        if baseline_slope > 0.0:
+            raise ValueError(
+                "`soft_label_baseline_slope` must be a negative floating point \
+                    or zero."
+            )
+
+        # Correct target labels with soft labels method
         if baseline_slope == 0.0:
             soft_labels = np.maximum(self._raw_labels * topline, baseline)
         else:
+            # Base adjusted considering age of the subject
+            # NOTE: The younger the greater uncertainty about its negative
+            #  label
             ages = np.minimum(np.maximum(self._ages, 20.0), 80.0)
             base_adjusted_by_age = np.maximum(
                 baseline + baseline_slope * (ages - 20.0) / (80.0 - 20.0), 0
             )
             soft_labels = np.maximum(self._raw_labels * topline, base_adjusted_by_age)
+
         return soft_labels
 
     def __len__(self) -> int:
+        """Returns the number of patients"""
         return len(self.labels)
 
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Gets encoded features and target labels"""
         return self.features[idx], self.labels[idx]
 
-    def ajust_soft_label(self, baseline, topline, baseline_slope) -> None:
-        labels = self._soft_label(baseline, topline, baseline_slope)
-        self.labels = torch.unsqueeze(torch.tensor(labels, dtype=torch.float), 1).to(
-            "cuda"
-        )
+    # FIXME: Seem unused -> Remove
+    # def adjust_soft_label(
+    #     self, baseline: float, topline: float, baseline_slope: float
+    # ) -> None:
+    #     labels = self._soft_label(baseline, topline, baseline_slope)
+    #     self.labels = torch.unsqueeze(torch.tensor(labels, dtype=torch.float), 1).to(
+    #         "cuda"
+    #     )
 
 
+# FIXME: Remove after creating tests
 if __name__ == "__main__":
-    DATASET = "../datasets/visits_sp_unique_train_positivo_1000_negative_0.csv"
-    # def get_feature_names(fn, BMI=False):
-    #     import pandas as pd
-    #     import numpy as np
-    #     col_names = pd.read_csv(fn).columns.values
-    #     # features = np.delete(col_names, ['id','BMI','T2D'])
-    #     if BMI:
-    #         features = list(np.setdiff1d(col_names, np.array(['id','T2D','mo','fa']), assume_unique=True))
-    #     else:
-    #         features = list(np.setdiff1d(col_names, np.array(['id','BMI','T2D','mo','fa']), assume_unique=True))
+    # Get path to positive-dataset
+    DATASET = "data/visits_sp_unique_train_positivo_1000_random_0.csv"
 
-    #     if "mo_t2d" in features and "fa_t2d" in features:
-    #         #put mo_t2d at the end and add 2 positions to n_feat (mo_t2d needs 3 positions in input vector)
-    #         features.remove("mo_t2d")
-    #         features.append("mo_t2d")
-    #         features.remove("fa_t2d")
-    #         features.append("fa_t2d")
-
-    #     return features
-
-    features = get_feature_names(DATASET, BMI=False)[991:]
+    # Extract features
+    features = get_feature_names(DATASET)
     # print(features)
-    dataset = DiabDataset(DATASET, features, soft_label=True)
-    print(dataset.features.shape)
-    print(dataset.labels.shape)
-    # print(dataset.raw_values.shape)
-    # print(dataset.raw_values[0])
-    print(dataset[0])
-    # print(dataset.features[-1])
-    # print(dataset.raw[-1])
 
-    # c = ['snp_{}'.format(i) for i in range(1,101)]
-    # c.append('AGE')
-    # d = DiabDataset('../datasets/train.csv', c, label='T2D', random_age=True)
-    # print(d.features)
-    # print(d.n_feat)
-    # print(d[1])
-    # print(len(d))
-    # print(d[0][0].shape)
+    # Create DiabDataset
+    dataset = DiabDataset(DATASET, features, soft_label=True)
+
+    # __getitem__
+    print(dataset[0])  # patient 0
+    print(dataset[0][0])  # features
+    print(dataset[0][1])  # label
+
+    # __len__
+    print(len(dataset))  # number of patients
+
+    # Display attributes and their shape
+    print(dataset.feat_names[0])  # features names
+    print(len(dataset.feat_names))  # shape of features names
+    print(dataset.n_feat)  # number of features
+    print(dataset.features[0])  # features
+    print(dataset.features.shape)  # shape of features
+    print(dataset.labels[0])  # labels
+    print(dataset.labels.shape)  # shape of labels
+    print(dataset._raw_values[0])  # raw feature values
+    print(dataset._raw_values.shape)  # shape of raw feature values
+    print(dataset._raw_labels[0])  # raw target labels
+    print(dataset._raw_labels.shape)  # shape of raw target labels
+    print(dataset._ages[0])  # ages
+    print(dataset._ages.shape)  # shape of ages
