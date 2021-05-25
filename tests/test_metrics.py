6cd7e308ec5f1bae3c8d062602fc7b10396d5161
jvsguerra <jvsguerra@gmail.com>
2021-05-25 11:53:35 -0300

    Update metrics testing and code

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 import torch
-from diabnet.metrics import ece, mce, ece_mce
+from diabnet.metrics import ece_mce
 
 
 class TestMetrics(unittest.TestCase):
@@ -9,14 +9,28 @@ class TestMetrics(unittest.TestCase):
         self.targets = torch.arange(0.0, 1.0, 0.01)
         self.predictions = torch.arange(0.0, 1.0, 0.01)
 
-    def test_ece(self):
-        result = ece(self.targets, self.predictions, 10)
-        self.assertEqual(result, 0.0)
+    # def test_ece(self):
+    #     result = ece(self.targets, self.predictions, 10)
+    #     self.assertEqual(result, 0.0)
 
-    def test_mce(self):
-        result = mce(self.targets, self.predictions, 10)
-        self.assertEqual(result, 0.0)
+    # def test_mce(self):
+    #     result = mce(self.targets, self.predictions, 10)
+    #     self.assertEqual(result, 0.0)
 
     def test_mce_ece(self):
+        # torch.Tensor inputs
         result = ece_mce(self.predictions, self.targets, 10)
         self.assertTupleEqual(result, (0.0, 0.0))
+
+    def test_bad_types(self):
+        # numpy.ndarray inputs
+        self.assertRaises(
+            TypeError, ece_mce, np.arange(0.0, 1.0, 0.01), np.arange(0.0, 1.0, 0.01)
+        )
+        # List inputs
+        self.assertRaises(
+            TypeError,
+            ece_mce,
+            np.arange(0.0, 1.0, 0.01).tolist(),
+            np.arange(0.0, 1.0, 0.01).tolist(),
+        )
